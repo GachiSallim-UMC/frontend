10b9fd9a5435e7a0d60bd2462464bbe1d81c056a
soyun0318 <thdbs0318@naver.com>
2026-07-18 01:13:00 +0900

    design: 이용약관 페이지 컴포넌트 구현

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,3 +1,5 @@
 export {LoginForm} from './components/LoginForm/LoginForm';
 export {SocialLoginForm} from './components/LoginForm/SocialLoginForm';
 export {SignupForm} from './components/SignupForm/SignupForm';
+
+export { TERMS_DATA } from './constants/Terms'
