61644ff02730afbc8a64f08ce461abd3ff691f9f
keumbyeol <bgbbgb1234@gmail.com>
2026-07-21 12:47:03 +0900

    design: 생활비 등록 및 정산 상세 UI 구현 (#34)
    * design: 생할비 등록 및 정상 상세 UI 구현
    
    * feat: 지출 등록 폼 입력값 및 상태 연동
    
    * fix: 타입 임포트 에러 수정
    
    * fix: 생활비 카테고리 목록 수정
    
    * fix: 정산 직접 입력창 정렬
    
    * fix: 훅 분리 및 라우터 수정
    
    * fix: 타입 오류 수정
    
    * fix: 린트 오류 수정
    
    * fix: 임포트 수정

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -5,6 +5,7 @@ import { ItemListPage, ItemFormPage, ItemEditRoute } from '@/pages/item';
 import { LoginPage } from '@/pages/auth/LoginPage';
 import { SignupPage } from '@/pages/auth/SignupPage';
 import { AppLayout } from './AppLayout';
+import { ExpenseListPage, ExpenseAddPage } from '@/pages/expense';
 import { NotificationPage } from '@/pages/notification';
 import { ChoreListPage } from '@/pages/chore/ChoreListPage';
 import { ChoreCreatePage } from '@/pages/chore/ChoreCreatePage';
@@ -22,10 +23,10 @@ export const router = createBrowserRouter([
       { path: '/dashboard', element: <PlaceholderPage title="대시보드" /> },
       { path: '/chores', element: <ChoreListPage /> },
       { path: '/chores/new', element: <ChoreCreatePage /> },
-      { path: '/chores/:id/edit', element: <ChoreEditPage /> },
-      { path: '/expenses', element: <PlaceholderPage title="생활비 정산" /> },
-      { path: '/expenses/new', element: <PlaceholderPage title="생활비 등록" /> },
-      { path: '/expenses/:id', element: <PlaceholderPage title="정산 상세" /> },
+      { path: '/chores/:id/edit', element: <ChoreEditPage /> },  
+      { path: '/expenses', element: <ExpenseListPage title="생활비 정산" /> },
+      { path: '/expenses/new', element: <ExpenseAddPage title="생활비 등록" /> },
+      { path: '/expenses/:id', element: <ExpenseAddPage title="정산 상세" /> },
       { path: '/items', element: <ItemListPage /> },
       { path: '/items/new', element: <ItemFormPage /> },
       { path: '/items/:id/edit', element: <ItemEditRoute /> },
